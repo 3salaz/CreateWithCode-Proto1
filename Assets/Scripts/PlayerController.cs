83606d732c69cf2a4e49cb83904d89d16c70d071
3salaz <3salaz.dev@gmail.com>
2020-11-18 11:26:09 -0800

    added user input to rotate and move vehical

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,27 +6,28 @@ public class PlayerController : MonoBehaviour
 {
     // access mofifier like (public) -- private means can only be used within this class  "playerConteroller"
     public float speed = 20.0f;
-    // Start is called before the first frame update
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+
+    void Movement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // moves forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); 
+        // rotate based on horizonal input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        MoveForward();
-        void MoveForward()
-        {
-            // 10m / sec speed
-            transform.Translate(Vector3.forward * Time.deltaTime * speed); 
-        }
-        
-        // MoveBackwards();
-        void MoveBackwards()
-        {
-            transform.Translate(Vector3.back); 
-        }
-        
+    { 
+        Movement();
     }
 }
